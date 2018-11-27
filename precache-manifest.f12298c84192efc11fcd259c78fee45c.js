7960da847c370360ff777001f26ddd2e321167f8
Lautaro Umpierrez <lauta.guillermo@gmail.com>
2018-11-27 00:30:34 -0300

    deploy

---
+++ precache-manifest.f12298c84192efc11fcd259c78fee45c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "295419102333d6e6b115b078f68aba3b",
+    "url": "/static/media/UziLogo.29541910.png"
+  },
+  {
+    "revision": "8c79fcd667f55875cc06",
+    "url": "/static/css/main.7ed78e17.chunk.css"
+  },
+  {
+    "revision": "77bb4c4832ffb14fb64ef0114bc146c6",
+    "url": "/static/media/cool.77bb4c48.svg"
+  },
+  {
+    "revision": "aa0cc10fb87def571154",
+    "url": "/static/js/1.aa0cc10f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "99a461e16024ad596977a6928891efc0",
+    "url": "/static/media/founder.sam.99a461e1.png"
+  },
+  {
+    "revision": "a17879467e3d5ce79b46323138957930",
+    "url": "/static/media/founder.lauti.a1787946.png"
+  },
+  {
+    "revision": "8c79fcd667f55875cc06",
+    "url": "/static/js/main.8c79fcd6.chunk.js"
+  },
+  {
+    "revision": "66365ed7309526c617d6b22553829bd7",
+    "url": "/static/media/play.66365ed7.svg"
+  },
+  {
+    "revision": "306eafe86bc8395180e78b1fb4894ddb",
+    "url": "/static/media/power.306eafe8.svg"
+  },
+  {
+    "revision": "b83011e65e2f6d424cb479f037857901",
+    "url": "/static/media/arrow-down.b83011e6.svg"
+  },
+  {
+    "revision": "79390f61401bd68da2cce7c0fb17cc2d",
+    "url": "/static/media/arrow-up.79390f61.svg"
+  },
+  {
+    "revision": "a72b9c7a793a0e4d5b1e75c30f81b004",
+    "url": "/static/media/smile.a72b9c7a.svg"
+  },
+  {
+    "revision": "aa0cc10fb87def571154",
+    "url": "/static/css/1.f64d8389.chunk.css"
+  },
+  {
+    "revision": "699db24f0af4c72fbd78218313948f39",
+    "url": "/index.html"
+  }
+];
